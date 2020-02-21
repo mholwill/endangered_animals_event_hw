934f039e647cb7252125b0718c4e101da6b1f0e8
mholwill <m_holwill@hotmail.co.uk>
2020-02-21 13:04:25 +0000

    event of form adding to website working

--- js/app.js
+++ js/app.js
@@ -1,4 +1,40 @@
 document.addEventListener('DOMContentLoaded', () =>{
-  
+
+  const newInput = document.querySelector('#new-form-item');
+  newInput.addEventListener('submit', handleItemSubmit);
 
 });
+
+const handleItemSubmit = function (event) {
+  event.preventDefault();
+
+  const animalListItem = createAnimalListItem(event.target);
+  const listOfAnimals = document.querySelector('#list-of-animals');
+  listOfAnimals.appendChild(animalListItem);
+
+  event.target.reset();
+
+};
+
+const createAnimalListItem = function (form) {
+  const animalListItem = document.createElement('li');
+  animalListItem.classList.add('animal-list-item');
+
+  const animal = document.createElement('h2');
+  animal.textContent = form.animal.value.toUpperCase();
+  animalListItem.appendChild(animal);
+
+  const type = document.createElement('p');
+  type.textContent = form.type.value;
+  animalListItem.appendChild(type);
+
+  const country = document.createElement('p');
+  country.textContent = form.country.value;
+  animalListItem.appendChild(country);
+
+  const severity = document.createElement('h2');
+  severity.textContent = form.severity.value;
+  animalListItem.appendChild(severity);
+
+  return animalListItem;
+}
